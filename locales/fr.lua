7df6fd55cea0fdd5136a5399166f8d38db095acd
LuaDeldu <luadelduwaht@outlook.fr>
2017-11-15 20:32:42 +0100

    Locale FIX

--- locales/fr.lua
+++ locales/fr.lua
@@ -34,7 +34,7 @@ Locales['fr'] = {
   ['shop']                     = 'Boutique du Vanilla Unicorn',
   ['shop_menu']                = 'Appuyez sur ~INPUT_CONTEXT~ pour accéder à la boutique.',
   ['bought']                   = 'Vous avez acheté ~b~1x ',
-  ['not_enough']               = 'Vous n\'avez ~r~pas assez~s~ d\'argent.',
+  ['not_enough_money']         = 'Vous n\'avez ~r~pas assez~s~ d\'argent.',
   ['max_item']                 = 'Vous en portez déjà assez sur vous.',
 
   -- Vehicles  
